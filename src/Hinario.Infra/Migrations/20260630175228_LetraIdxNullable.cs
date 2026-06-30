fbb47ff970d5c57d07d922e08ea4ae9a9a80a7d9
raissaduarteb <raissadbraga@gmail.com>
2026-06-30 14:53:39 -0300

    Ajustes

---
+++ src/Hinario.Infra/Migrations/20260630175228_LetraIdxNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NpgsqlTypes;
+
+#nullable disable
+
+namespace Hinario.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class LetraIdxNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<NpgsqlTsVector>(
+                name: "letra_idx",
+                table: "hinos",
+                type: "tsvector",
+                nullable: true,
+                oldClrType: typeof(NpgsqlTsVector),
+                oldType: "tsvector");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<NpgsqlTsVector>(
+                name: "letra_idx",
+                table: "hinos",
+                type: "tsvector",
+                nullable: false,
+                oldClrType: typeof(NpgsqlTsVector),
+                oldType: "tsvector",
+                oldNullable: true);
+        }
+    }
+}
